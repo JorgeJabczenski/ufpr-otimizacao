ca10711237e2c17b01be7a8227dfea12a422a67a
tikaradate <tikaradate@gmail.com>
2021-12-05 14:36:42 -0300

    Comentado

--- t2/src/quimica.cpp
+++ t2/src/quimica.cpp
@@ -12,12 +12,14 @@ using namespace std;
 /** ESTRUTURAS **/
 /****************/
 
-typedef struct objeto_s {
+typedef struct objeto_s 
+{
     int peso;
     int valor;
 } objeto_t;
 
-typedef struct par_s{
+typedef struct par_s
+{
     int a;
     int b;
 } par_t;
@@ -42,43 +44,52 @@ par_t *pares;          //
 /**      Funções      **/
 /***********************/
 
-objeto_t *alocaObjetos(int n){
+objeto_t *alocaObjetos(int n)
+{
     return (objeto_t *) malloc (n*sizeof(objeto_t));
 }
-void lerObjetos(int n, objeto_t *objetos){
+
+void lerObjetos(int n, objeto_t *objetos)
+{
     for (int i = 0; i < n; i++) scanf("%d", &objetos[i].peso);
     for (int i = 0; i < n; i++) scanf("%d", &objetos[i].valor);
     
 }
-void imprimeObjeto(objeto_t objeto){
+
+void imprimeObjeto(objeto_t objeto)
+{
     printf("peso: %d \t| valor: %d\n", objeto.peso, objeto.valor);
 }
 
-par_t *alocaPares(int n){
+par_t *alocaPares(int n)
+{
     return (par_t *) malloc (n*sizeof(par_t));
 }
-void lerPares(int n, par_t *pares){
+
+void lerPares(int n, par_t *pares)
+{
     for (int i = 0; i < n; i++) scanf("%d %d",&pares[i].a, &pares[i].b);
 }
-void imprimePar(par_t par){
+
+void imprimePar(par_t par)
+{
     printf("a: %d | b: %d\n", par.a, par.b);
 }
 
-bool cmp (const objeto_t& objeto1, const objeto_t& objeto2){
+// função comparadora para o problema da mochila racional
+bool cmp (const objeto_t& objeto1, const objeto_t& objeto2)
+{
     return((double)objeto1.valor/objeto1.peso) > ((double)objeto2.valor/objeto2.peso);
 }
 
-void copiaVetores(int *dst, int *scr){
-    memcpy(dst, scr, sizeof(*dst));
-}
-
-void copiaEscolhas(){
+void copiaEscolhas()
+{
     for (int i = 0; i < qtdObjetos; i++)
         escolhasOtimas[i] = escolhasAtuais[i];
 }
 
-double rational_knapsack(objeto_t *objetos, int inicio, int pesoRestante){
-
+double rationalKnapsack(objeto_t *objetos, int inicio, int pesoRestante)
+{
     int tamanhoAtual = qtdObjetos - inicio;
     double peso = 0;
     double valor = 0;
@@ -109,22 +120,25 @@ double rational_knapsack(objeto_t *objetos, int inicio, int pesoRestante){
 }
 
 // checa se o próximo item a ser colocado na mochila reage com um item
-bool reage(int *estado_atual, int item_teste){
+bool reage(int *estadoAtual, int itemTeste)
+{
     for (int i = 0; i < qtdPares; i++){
-        if ((pares[i].a-1 == item_teste) && (estado_atual[pares[i].b-1] == 1)){
+        if ((pares[i].a-1 == itemTeste) && (estadoAtual[pares[i].b-1] == 1)){
             return true;
         }
-        if ((pares[i].b-1 == item_teste) && (estado_atual[pares[i].a-1] == 1)){
+        if ((pares[i].b-1 == itemTeste) && (estadoAtual[pares[i].a-1] == 1)){
             return true;
         }
     }
     return false;
 }
 
-void mochila_quimica(int tamanhoAtual, int pesoAtual){
-    
+void mochilaQuimica(int tamanhoAtual, int pesoAtual)
+{
     vertices++;
     
+    // chegou numa folha da árvore, então checa se este resultado é ótimo
+    // guardando as informações se for o caso
     if (tamanhoAtual == qtdObjetos){
         int lucroAtual = 0;
         for(int i = 0; i < qtdObjetos; i++){
@@ -143,32 +157,36 @@ void mochila_quimica(int tamanhoAtual, int pesoAtual){
         for(int i = 0; i < tamanhoAtual; i++){
             lucroAtual += objetos[i].valor*escolhasAtuais[i];
         }
+        // define as próximas escolhas, colocar ou não colocar o próximo item
         int proxEscolha[2] = {0, 1};
         double proxLimite[2];
-        
-        proxLimite[0] = lucroAtual + rational_knapsack(objetos, tamanhoAtual+1, pesoMaximo - pesoAtual);
         double proximoLucro = objetos[tamanhoAtual].valor;
         int     proximoPeso = objetos[tamanhoAtual].peso;
-        proxLimite[1]  = lucroAtual + proximoLucro + rational_knapsack(objetos, tamanhoAtual+1, pesoMaximo - pesoAtual - proximoPeso);
+        
+        // calcula os limites das próximas escolhas
+        proxLimite[0] = lucroAtual + rationalKnapsack(objetos, tamanhoAtual+1, pesoMaximo - pesoAtual);
+        proxLimite[1]  = lucroAtual + proximoLucro + rationalKnapsack(objetos, tamanhoAtual+1, pesoMaximo - pesoAtual - proximoPeso);
 
+        // ordena os limites encontrados
         if(proxLimite[1] > proxLimite[0]){
             swap(proxEscolha[0], proxEscolha[1]);
             swap(proxLimite[0], proxLimite[1]);
         }
-         
+        
+
         for (int i = 0; i < 2; i++){
             if (proxLimite[i] < lucroMaximo){
                 return;
             }
 
             int pesoItemAtual = objetos[tamanhoAtual].peso;
+            // se colocado o item e ele reage com algum da mochila ou ultrapassa o peso, não é avançado este galho da árvore
             if (proxEscolha[i] == 1 && (reage(escolhasAtuais, tamanhoAtual) || (pesoAtual + pesoItemAtual > pesoMaximo))){
                 continue;
             }
             escolhasAtuais[tamanhoAtual] = proxEscolha[i];
-            mochila_quimica(tamanhoAtual+1, pesoAtual + pesoItemAtual*proxEscolha[i]);
+            mochilaQuimica(tamanhoAtual+1, pesoAtual + pesoItemAtual*proxEscolha[i]);
         }
-
     }
 }
 
@@ -202,7 +220,7 @@ main()
     lucroMaximo = 0;
     vertices = 0;
     tempo = timestamp();
-    mochila_quimica(0,0);
+    mochilaQuimica(0,0);
     tempo = timestamp() - tempo;
     cout <<  lucroMaximo << endl;
     // imprime os itens escolhidos
